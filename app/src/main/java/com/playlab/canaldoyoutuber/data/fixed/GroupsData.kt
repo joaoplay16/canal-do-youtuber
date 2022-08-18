09aeb6e1d1c75f49effe5a9c6832e03714755a61
João Pedro <joaoplay16@gmail.com>
2022-08-17 21:15:57 -0300

    GroupsData created

---
+++ app/src/main/java/com/playlab/canaldoyoutuber/data/fixed/GroupsData.kt
@@ -0,0 +1,68 @@
+package com.playlab.canaldoyoutuber.data.fixed
+
+import com.playlab.canaldoyoutuber.R
+import com.playlab.canaldoyoutuber.model.Group
+
+/**
+ * Contém os dados dos grupos, de outras redes,
+ * vinculados ao canal YouTube do aplicativo.
+ *
+ * O objetivo desta classe é trabalhar como uma
+ * persistência local estática, fixa, que contém
+ * os dados dos grupos.
+ *
+ * Como esses dados tendem a sofrer poucas
+ * alterações (incluindo a inserção de novos grupos)
+ * e com espaços de tempo longos entre as alterações,
+ * então a melhor escolha foi o trabalho deles em
+ * uma classe estática (companion object) que
+ * trabalha como se fosse uma persistência de dados
+ * estáticos.
+ */
+
+abstract class GroupsData {
+    companion object {
+        /**
+         * Retorna todos os grupos vinculados ao
+         * canal.
+         *
+         * @return lista não mutável de objetos
+         * [Group].
+         */
+        fun getGroups() = listOf(
+            Group(
+                place = "Facebook",
+                name = "Desenvolvimento Mobile",
+                webUri = "https://www.facebook.com/groups/" +
+                        "246149505467359",
+                logo = R.drawable.ic_facebook_group_color
+            ),
+            Group(
+                place = "WhatsApp",
+                name = "\uD83D\uDCAF% Android",
+                webUri = "https://chat.whatsapp.com/" +
+                        "HLXgiKgC6o96q8UWGjiidW",
+                logo = R.drawable.ic_whatsapp_color
+            ),
+            Group(
+                place = "Telegram",
+                name = "DevCodeBr - Android/Java/Kotlin",
+                webUri = "https://t.me/devcodebr_android",
+                logo = R.drawable.ic_telegram_color
+            ),
+            Group(
+                place = "Discord",
+                name = "Android Discord",
+                webUri = "https://discord.gg/B8XEDGC",
+                logo = R.drawable.ic_discord_color
+            ),
+            Group(
+                place = "LinkedIn",
+                name = "Android Developers Brazil",
+                webUri = "https://www.linkedin.com/groups/" +
+                        "4447810/",
+                logo = R.drawable.ic_linkedin_group_color
+            )
+        )
+    }
+}
