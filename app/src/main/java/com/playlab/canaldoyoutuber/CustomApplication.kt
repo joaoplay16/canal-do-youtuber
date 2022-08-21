96bfac10f9989da1e5b197c2d767d420182cacf0
João Pedro <joaoplay16@gmail.com>
2022-08-20 21:00:28 -0300

    Application class crated

---
+++ app/src/main/java/com/playlab/canaldoyoutuber/CustomApplication.kt
@@ -0,0 +1,57 @@
+package com.playlab.canaldoyoutuber
+
+import android.app.Application
+import com.onesignal.OneSignal
+import com.playlab.canaldoyoutuber.config.OneSignalConfig
+
+/**
+ * Classe de sistema e de objeto único enquanto
+ * o aplicativo estiver em execução.
+ *
+ * Com essa característica está é a classe
+ * responsável por iniciar as entidades de
+ * WorkManager (trabalho em background) e
+ * OneSignal (notificação push). Entidades essas
+ * que precisam ser invocadas logo no início da
+ * execução do aplicativo ainda na primeira vez
+ * que ele é acessado pelo usuário.
+ *
+ * Note que para essa classe ser invocada pelo
+ * sistema ela precisa estar configurada no
+ * AndroidManifest.xml como:
+ *
+ * <application
+ *
+android:name=".CustomApplication"
+ *
+...>
+ *
+ * @constructor cria um objeto completo do tipo
+ * [CustomApplication].
+ */
+class CustomApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        oneSignalInit()
+    }
+    /**
+    * Inicializa o OneSignal e registra o
+    * usuário do app para que ele já consiga
+    * receber as notificações push do canal
+    * do aplicativo.
+    *
+    * Com a configuração a seguir é preciso
+    * que também tenha definido no aplicativo
+    * um serviço do tipo
+    * [NotificationExtenderService] para que as
+    * notificações push sejam interceptadas
+    * e trabalhadas de maneira personalizada.
+    */
+    private fun oneSignalInit() {
+        // Enable verbose OneSignal logging to debug issues if needed.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(OneSignalConfig.App.ID);
+    }
+}
