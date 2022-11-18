ab428f05ebb6313662f88ad5570f86f9f39ee5ca
João Pedro <joaoplay16@gmail.com>
2022-11-18 12:08:58 -0300

    test click on social networks menu item

---
+++ app/src/androidTest/java/com/playlab/canaldoyoutuber/ui/TestMainActivity.kt
@@ -0,0 +1,38 @@
+package com.playlab.canaldoyoutuber.ui
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.google.common.truth.Truth.assertThat
+import com.playlab.canaldoyoutuber.R
+import com.playlab.canaldoyoutuber.ui.adapter.MenuViewHolder
+import com.playlab.canaldoyoutuber.ui.fragment.SocialNetworksFragment
+import org.junit.Rule
+import org.junit.Test
+
+class TestMainActivity {
+
+    @get:Rule
+    var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun clickSocialNetworksMenuItem_showSocialNetworksFragment (){
+
+        onView(withId(R.id.rv_menu)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<MenuViewHolder>(
+                1,
+                click()
+            )
+        )
+        activityScenarioRule.scenario.onActivity { activity ->
+            val currentFragment = activity
+                    .supportFragmentManager
+                    .findFragmentByTag(SocialNetworksFragment.KEY)
+
+            assertThat(currentFragment).isNotNull()
+            assertThat(currentFragment?.isVisible).isTrue()
+        }
+    }
+}
