5252bb685567776ed508061b424ec563fb5c0cce
João Pedro <joaoplay16@gmail.com>
2022-11-30 21:00:44 -0300

    test click on about menu item the AboutChannelFragment should be shown

--- app/src/androidTest/java/com/playlab/canaldoyoutuber/ui/TestMainActivity.kt
+++ app/src/androidTest/java/com/playlab/canaldoyoutuber/ui/TestMainActivity.kt
@@ -8,6 +8,7 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.google.common.truth.Truth.assertThat
 import com.playlab.canaldoyoutuber.R
 import com.playlab.canaldoyoutuber.ui.adapter.MenuViewHolder
+import com.playlab.canaldoyoutuber.ui.fragment.AboutChannelFragment
 import com.playlab.canaldoyoutuber.ui.fragment.GroupsFragment
 import com.playlab.canaldoyoutuber.ui.fragment.PlayListsFragment
 import com.playlab.canaldoyoutuber.ui.fragment.SocialNetworksFragment
@@ -70,6 +71,25 @@ class TestMainActivity {
             val currentFragment = activity
                 .supportFragmentManager
                 .findFragmentByTag(GroupsFragment.KEY)
+
+            assertThat(currentFragment).isNotNull()
+            assertThat(currentFragment?.isVisible).isTrue()
+        }
+    }
+
+    @Test
+    fun clickAboutMenuItem_showAboutFragment (){
+
+        onView(withId(R.id.rv_menu)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<MenuViewHolder>(
+                4,
+                click()
+            )
+        )
+        activityScenarioRule.scenario.onActivity { activity ->
+            val currentFragment = activity
+                .supportFragmentManager
+                .findFragmentByTag(AboutChannelFragment.KEY)
 
             assertThat(currentFragment).isNotNull()
             assertThat(currentFragment?.isVisible).isTrue()
